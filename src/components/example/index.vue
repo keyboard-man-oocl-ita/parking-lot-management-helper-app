6d9872081474b59246d8cd2d402e93983f95fe15
Paulljc <714978398@qq.com>
2019-07-27 19:15:30 +0800

    [LJC]:add mint-ui and init index.vue

--- src/components/example/index.vue
+++ src/components/example/index.vue
@@ -1,12 +1,47 @@
 <template>
-  <span>Hello world</span>
+  <div>
+    <mt-header fixed title="P.L.M.H">
+      <div slot="left">
+        <mt-button v-link="'/'" icon="back">返回</mt-button>
+      </div>
+      <div slot="right">
+        <mt-button @click="handleClose">关闭</mt-button>
+      </div>
+    </mt-header>
+
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="tab1">
+        <img slot="icon" src="../../assets/list.svg" />
+        抢单
+      </mt-tab-item>
+      <mt-tab-item id="tab2">
+        <img slot="icon" src="../../assets/car.svg" />
+        停取
+      </mt-tab-item>
+      <mt-tab-item id="tab3">
+        <img slot="icon" src="../../assets/history.svg" />
+        历史
+      </mt-tab-item>
+      <mt-tab-item id="tab4">
+        <img slot="icon" src="../../assets/person.svg" />
+        个人
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ExampleComponent',
+  name: "ExampleComponent",
   data() {
-    return {}
+    return {
+      selected: ""
+    };
+  },
+  methods:{
+    handleClose(){
+      
+    }
   }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+Vue.use(MintUI)
 
 import router from './router/index'
 
