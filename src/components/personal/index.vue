e72b21d12a15d38ad89cd12ce1ab250b50b7e46a
FELIX WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGFE@OOCL.COM>
2019-07-28 16:40:02 +0800

    [Felix]: change menu label to Chinese in personal component

--- src/components/personal/index.vue
+++ src/components/personal/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <Header :title="'个人信息'"></Header>
-    <mt-field label="phone_number" type="tel" readonly value="13866668888" disableClear></mt-field>
-    <mt-field label="email" type="email" readonly value="clerk@oocl.com" disableClear></mt-field>
-    <mt-field label="role" readonly value="Parking Boy" disableClear></mt-field>
-    <mt-field label="status" readonly value="Active" disableClear></mt-field>
-    <mt-field label="managed_by" readonly value="Parking Manager" disableClear></mt-field>
+    <mt-field label="手机号" type="tel" readonly value="13866668888" disableClear></mt-field>
+    <mt-field label="邮箱" type="email" readonly value="clerk@oocl.com" disableClear></mt-field>
+    <mt-field label="角色" readonly value="Parking Boy" disableClear></mt-field>
+    <mt-field label="冻结状态" readonly value="Active" disableClear></mt-field>
+    <mt-field label="直属上司" readonly value="Parking Manager" disableClear></mt-field>
   </div>
 </template>
 
