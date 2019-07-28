897f8bf15b5089486ef951761df949422711e425
FELIX WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGFE@OOCL.COM>
2019-07-28 16:19:26 +0800

    [Felix]: init personal component

--- src/components/personal/index.vue
+++ src/components/personal/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <Header :title="'个人信息'"></Header>This is personal component
+    <Header :title="'个人信息'"></Header>
+    <mt-field label="phone_number" type="tel" readonly="true" value="13866668888"></mt-field>
+    <mt-field label="email" type="email" readonly="true" value="clerk@oocl.com"></mt-field>
+    <mt-field label="role" readonly="true" value="Parking Boy"></mt-field>
+    <mt-field label="status" readonly="true" value="Active"></mt-field>
+    <mt-field label="managed_by" readonly="true" value="Parking Manager"></mt-field>
   </div>
 </template>
 
