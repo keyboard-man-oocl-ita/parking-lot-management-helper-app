3ddf540414e524bbeec7a34c038fc7f864e56452
alauhy <751961605@qq.com>
2019-07-28 22:15:10 +0800

    [LXY]:implement parking clerk orders page

--- src/components/parkAndFetch/index.vue
+++ src/components/parkAndFetch/index.vue
@@ -1,44 +1,94 @@
 <template>
-  <div>
-    <Header :title="'正在进行的订单'"></Header>
+    <div>
+        <Header :title="'正在进行的订单'"></Header>
 
-    <el-table :data="ordering" stripe style="width: 100%" size="small">
-      <el-table-column prop="orderNumber" label="订单号" width="80"></el-table-column>
-      <el-table-column prop="carLicense" label="车牌号" width="80"></el-table-column>
-      <el-table-column prop="parkinglot" label="停车场"></el-table-column>
-      <el-table-column prop="status" label="状态"></el-table-column>
-    </el-table>
+        <el-table :data="orders" stripe style="width: 100%" size="small">
+            <el-table-column prop="orderId" label="订单号" width="80"></el-table-column>
+            <el-table-column prop="carLicense" label="车牌号" width="80"></el-table-column>
+            <el-table-column prop="parkinglot" label="停车场"></el-table-column>
+            <el-table-column prop="status" :formatter="getStatus" label="状态"></el-table-column>
+        </el-table>
 
-  </div>
+    </div>
 </template>
 
 <script>
-import Header from "@/components/header/index";
+    import Header from '@/components/header/index';
+    import {mapState} from 'vuex'
 
-export default {
-  name: "parkAndFetch",
-  components: {
-    Header
-  },
-  data() {
-    return {
-      ordering: [
-        {
-          orderNumber: "454545",
-          carLicense: "粤A 88888",
-          parkinglot: "yangming",
-          status: "已接单"
+    const FINISHED = 3;
+    const PARKED = 2;
+    const RECEIPTED = 1;
+    const SNATCHING = 0;
+    export default {
+        name: "parkAndFetch",
+        components: {
+            Header
         },
-        {
-          orderNumber: "454545",
-          carLicense: "粤B 88888",
-          parkinglot: "yangming",
-          status: "未接单"
+        computed:mapState({
+
+            orders : state => state.clerkOrderList
+
+        }),
+        data() {
+            return {
+                // ordering: [
+                //     {
+                //         orderId: "454545",
+                //         carLicense: "粤A 88888",
+                //         parkinglot: "yangming",
+                //         status: 1
+                //     },
+                //     {
+                //         orderId: "454545",
+                //         carLicense: "粤B 88888",
+                //         parkinglot: "yangming",
+                //         status: 2
+                //     },
+                //     {
+                //         orderId: "454545",
+                //         carLicense: "粤A 88888",
+                //         parkinglot: "yangming",
+                //         status: 1
+                //     },
+                //     {
+                //         orderId: "454545",
+                //         carLicense: "粤B 88888",
+                //         parkinglot: "yangming",
+                //         status: 0
+                //     },
+                //     {
+                //         orderId: "454545",
+                //         carLicense: "粤A 88888",
+                //         parkinglot: "yangming",
+                //         status: 1
+                //     },
+                //     {
+                //         orderId: "454545",
+                //         carLicense: "粤B 88888",
+                //         parkinglot: "yangming",
+                //         status: 2
+                //     }
+                // ]
+            };
+        },
+        methods: {
+            getStatus(row, column, cellValue) {
+
+                if (cellValue === SNATCHING) {
+                    return '抢单中'
+                } else if (cellValue === RECEIPTED) {
+                    return '已接单'
+                } else if (cellValue === PARKED) {
+                    return '已停车'
+                } else if (cellValue === FINISHED) {
+                    return '已完成'
+                }
+
+
+            }
         }
-      ]
     };
-  }
-};
 </script>
 
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
 
+    clerkOrderList:[]
+
+  },
+  mutations:{
+    setOrderList(state,list){
+      state.clerkOrderList = list
+    }
+  },
+  actions:{
+    getOrderList({commit}){
+      axios.get("http://localhost:8080/orders")
+          .then(res=>{
+            commit('setOrderList',res.data)
+          })
+          .catch(error=>console.log(error));
+    }
   }
 })
 
