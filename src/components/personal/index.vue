678e1a7464d5e3f8a22ccff8afa507ba14449ee4
FELIX WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGFE@OOCL.COM>
2019-07-28 16:30:37 +0800

    [Felix]: solved the console error

--- src/components/personal/index.vue
+++ src/components/personal/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <Header :title="'个人信息'"></Header>
-    <mt-field label="phone_number" type="tel" readonly="true" value="13866668888" disableClear="true"></mt-field>
-    <mt-field label="email" type="email" readonly="true" value="clerk@oocl.com" disableClear="true"></mt-field>
-    <mt-field label="role" readonly="true" value="Parking Boy" disableClear="true"></mt-field>
-    <mt-field label="status" readonly="true" value="Active" disableClear="true"></mt-field>
-    <mt-field label="managed_by" readonly="true" value="Parking Manager" disableClear="true"></mt-field>
+    <mt-field label="phone_number" type="tel" readonly value="13866668888" disableClear></mt-field>
+    <mt-field label="email" type="email" readonly value="clerk@oocl.com" disableClear></mt-field>
+    <mt-field label="role" readonly value="Parking Boy" disableClear></mt-field>
+    <mt-field label="status" readonly value="Active" disableClear></mt-field>
+    <mt-field label="managed_by" readonly value="Parking Manager" disableClear></mt-field>
   </div>
 </template>
 
