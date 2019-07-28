9c216884d067b57ce54b22917c31fa722f633bb9
FELIX WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGFE@OOCL.COM>
2019-07-28 16:21:39 +0800

    [Felix]: update personal component

--- src/components/personal/index.vue
+++ src/components/personal/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <Header :title="'个人信息'"></Header>
-    <mt-field label="phone_number" type="tel" readonly="true" value="13866668888"></mt-field>
-    <mt-field label="email" type="email" readonly="true" value="clerk@oocl.com"></mt-field>
-    <mt-field label="role" readonly="true" value="Parking Boy"></mt-field>
-    <mt-field label="status" readonly="true" value="Active"></mt-field>
-    <mt-field label="managed_by" readonly="true" value="Parking Manager"></mt-field>
+    <mt-field label="phone_number" type="tel" readonly="true" value="13866668888" disableClear="true"></mt-field>
+    <mt-field label="email" type="email" readonly="true" value="clerk@oocl.com" disableClear="true"></mt-field>
+    <mt-field label="role" readonly="true" value="Parking Boy" disableClear="true"></mt-field>
+    <mt-field label="status" readonly="true" value="Active" disableClear="true"></mt-field>
+    <mt-field label="managed_by" readonly="true" value="Parking Manager" disableClear="true"></mt-field>
   </div>
 </template>
 
