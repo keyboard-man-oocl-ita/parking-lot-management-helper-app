a12a663c331a65d92f902bd33da6e1cf6165ce43
Felix Wang <742863722@qq.com>
2019-07-28 11:07:04 +0800

    add chooseParkingLot component

--- src/components/chooseParkingLot/index.vue
+++ src/components/chooseParkingLot/index.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        This is chooseParkingLot component
+        <Header :title="'选择停车场'"></Header>
+        <mt-cell v-for="(parkingLot,index) in parkingLots" :key="index" :title="parkingLot.name" to="/chooseParkingLot" is-link :label="`剩余空位：${parkingLot.remaining}`">
+            <span>选择</span>
+            <img slot="icon" src="../../assets/parkingLot-icon.svg" width="24" height="24" />
+        </mt-cell>
     </div>
 </template>
 
 <script>
+    import Header from '../header/index'
     export default {
-        name: "index.vue"
+        name: "index.vue",
+        components: {
+            Header
+        },
+        data() {
+            return {
+                remaining: 10,
+                parkingLots: [{
+                    name: '停车场一号',
+                    remaining: 10
+                },{
+                    name: '停车场二号',
+                    remaining: 9
+                },{
+                    name: '停车场三号',
+                    remaining: 5
+                },{
+                    name: '停车场四号',
+                    remaining: 2
+                },]
+            }
+        }
     }
 </script>
 
