f6cef4a0af502d8f1d4d60e757866abd48490dad
Paulljc <714978398@qq.com>
2019-07-28 11:58:26 +0800

    [gukson]:add axios and create api request.

--- src/components/grabOrder/index.vue
+++ src/components/grabOrder/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <Header :title="'订单'"></Header>
-    <mt-cell title="车牌号" to="/chooseParkingLot" is-link icon="../../assets/car_order.svg" label="停车时间：">
-      <span>抢单</span>
+    <mt-cell :title="`${item.carLicense}`" v-for="(item, index) in orders" :key="index" to="/chooseParkingLot" value="抢单" is-link :label="`停车时间：${item.parkTime}`" >
       <img slot="icon" src="../../assets/car_order.svg" width="24" height="24" />
     </mt-cell>
   </div>
@@ -10,15 +9,40 @@
 
 <script>
 import Header from '../header/index'
+import { setTimeout } from 'timers';
 
 export default {
   name: "grabOrder",
+  created(){
+    // this.fetchOrder()
+  },
   data(){
       return{
+          orders:[
+              {
+                carLicense: "粤A 88888",
+                parkTime: "19:00"
+              },
+              {
+                carLicense: "粤B 88888",
+                parkTime: "19:00"
+              },
+              {
+                carLicense: "粤C 88888",
+                parkTime: "19:00"
+              }
+          ],
       }
   },
   components:{
       Header
+  },
+  methods:{
+      fetchOrder(){
+          setTimeout(() => {
+              
+          }, 3000);
+      }
   }
 };
 </script>
