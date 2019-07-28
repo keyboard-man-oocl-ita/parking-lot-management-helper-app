fd66debf2a7a6b3e9203a43b4115a265157cd93d
Paulljc <714978398@qq.com>
2019-07-28 18:11:15 +0800

    [gukson]:change grab order logic.

--- src/components/grabOrder/index.vue
+++ src/components/grabOrder/index.vue
@@ -1,23 +1,25 @@
 <template>
   <div>
     <Header :title="'订单'"></Header>
-    <mt-cell
-      :title="`${item.carLicense}`"
-      v-for="(item, index) in orders"
-      :key="index"
-      to="/chooseParkingLot"
-      value="抢单"
-      is-link
-      :label="`停车时间：${item.parkTime}`"
-    >
-      <img slot="icon" src="../../assets/car_order.svg" width="24" height="24" />
-    </mt-cell>
+    <div @click="grabOrder">
+      <mt-cell
+        :title="`${item.carLicense}`"
+        v-for="(item, index) in orders"
+        :key="index"
+        value="抢单"
+        is-link
+        :label="`停车时间：${item.parkTime}`"
+      >
+        <img slot="icon" src="@/assets/car_order.svg" width="24" height="24" />
+      </mt-cell>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/header/index";
 import { fetchOrders } from "@/api/grabOrder";
+import { MessageBox, Toast } from "mint-ui";
 
 export default {
   name: "grabOrder",
@@ -41,7 +43,7 @@ export default {
         }
       ],
       parkingboy: {
-          id: '1'
+        id: "1"
       }
     };
   },
@@ -53,6 +55,28 @@ export default {
       setTimeout(() => {
         fetchOrders(this.parkingboy.id);
       }, 3000);
+    },
+    grabOrder() {
+      MessageBox.confirm("是否抢单", "提示").then(
+        action => {
+          if (action == "confirm") {
+            this.$router.push("/parkAndFetch");
+            Toast({
+              message: "抢单成功",
+              duration: 1000
+            });
+          }
+        },
+        action => {
+          if (action == "cancel") {
+            Toast({
+              message: "已取消",
+              position: "bottom",
+              duration: 1000
+            });
+          }
+        }
+      );
     }
   }
 };
