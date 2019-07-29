48f6bf0042afa8fee763fd7d4cd7f389ba88292c
Paulljc <714978398@qq.com>
2019-07-29 11:50:22 +0800

    [gukson]:refactor store

--- src/components/history/index.vue
+++ src/components/history/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
     <MyHeader :title="'历史'"></MyHeader>
-    
-    <mt-cell
-      :title="`${item.carLicense}`"
-      v-for="(item, index) in orders"
-      :key="index"
-      to="/details"
-      value="详细信息"
-      is-link
-      :label="`${item.createTime} - ${item.endTime}`"
-    >
-      <img slot="icon" src="@/assets/complete.svg" width="24" height="24" />
-    </mt-cell>
 
+    <div v-for="(item, index) in orders" :key="index">
+      <mt-cell
+        :title="`${item.carLicense}`"
+        :to="`/details/${item.id}`"
+        value="详细信息"
+        is-link
+        :label="`${item.createTime} - ${item.endTime}`"
+      >
+        <img slot="icon" src="@/assets/complete.svg" width="24" height="24" />
+      </mt-cell>
+    </div>
   </div>
 </template>
 
@@ -25,9 +24,11 @@ export default {
   components: {
     MyHeader
   },
-  data(){
+  mounted() {
+    // this.$store.dispatch("");
+  },
+  data() {
     return {
-      data:'www',
       orders: [
         {
           carLicense: "粤A 88888",
@@ -38,15 +39,11 @@ export default {
           carLicense: "粤B 88888",
           createTime: "19:00",
           endTime: "20:00"
-        },
-        {
-          carLicense: "粤C 88888",
-          createTime: "19:00",
-          endTime: "20:00"
         }
       ]
-    }
-  }
+    };
+  },
+  methods: {}
 };
 </script>
 
