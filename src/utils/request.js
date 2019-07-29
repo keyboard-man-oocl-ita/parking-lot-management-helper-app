4dee9d5451f4d400acb9b7f2277de07d02b0ceaf
FELIX WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGFE@OOCL.COM>
2019-07-29 20:39:58 +0800

    [Felix]: test CI/CD

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import store from '@/store'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: '/api/', // url = base url + request url
+  baseURL: 'http://39.100.32.147/api/', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
